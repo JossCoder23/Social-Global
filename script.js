36ba2fa9b9dd878422f093cb7d47e37762a7df40
Jose Pardo <jose.pardo@upch.pe>
2025-10-23 17:40:22 -0500

    update0540231025

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
 
 window.addEventListener("load", () => {
 
-    let carreras = document.getElementById('career'); //- ACA
-    let carrera_hidden = document.querySelector('.carreras__hidden');
-    let checkTutor = document.getElementById('tutor');
-    let inputTutor = document.querySelectorAll('.input__tutor');
-    let doc = document.getElementById('LEADCF22');
-    let program = document.getElementById('LEADCF220');
+    // let carreras = document.getElementById('career'); //- ACA
+    // let carrera_hidden = document.querySelector('.carreras__hidden');
+    // let checkTutor = document.getElementById('tutor');
+    // let inputTutor = document.querySelectorAll('.input__tutor');
+    // let doc = document.getElementById('LEADCF22');
+    // let program = document.getElementById('LEADCF220');
 
-    let modular = document.getElementById('LEADCF204');
-    let err = document.getElementById('container__error');
+    // let modular = document.getElementById('LEADCF204');
+    // let err = document.getElementById('container__error');
 
-    modular.addEventListener('change', (ev) => {
+    // modular.addEventListener('change', (ev) => {
 
-        const result = data.find(item => item.codigo_modular === modular.value);
+    //     const result = data.find(item => item.codigo_modular === modular.value);
 
-        if (result !== undefined) {
-            err.style.display = "none";
-            inputCategoria.value = result.categoria;
-            inputDistrito.value = result.distrito;
-            // inputColegio.value = result.colegio;
-            // inputTerm.value = result.categoria;
+    //     if (result !== undefined) {
+    //         err.style.display = "none";
+    //         inputCategoria.value = result.categoria;
+    //         inputDistrito.value = result.distrito;
+    //         // inputColegio.value = result.colegio;
+    //         // inputTerm.value = result.categoria;
 
-            inputDisabledColegio.value = result.colegio;
-            inputDisabledDistritoColegio.value = result.distrito;
+    //         inputDisabledColegio.value = result.colegio;
+    //         inputDisabledDistritoColegio.value = result.distrito;
 
-        } else {
-            err.style.display = "block";
-        }
-    })
+    //     } else {
+    //         err.style.display = "block";
+    //     }
+    // })
 
-    let tutoresCt = document.querySelector(".tutores");
-    checkTutor.addEventListener("change", () => {
-        if (checkTutor.checked) {
-            tutoresCt.style.display = "grid";
-            inputTutor.forEach(input => {
-                input.style.display = "block"
-            })
-        } else {
-            tutoresCt.style.display = "none";
-            inputTutor.forEach(input => {
-                input.style.display = "none";
-                input.value = "";
-            })
-        }
-    })
+    // let tutoresCt = document.querySelector(".tutores");
+    // checkTutor.addEventListener("change", () => {
+    //     if (checkTutor.checked) {
+    //         tutoresCt.style.display = "grid";
+    //         inputTutor.forEach(input => {
+    //             input.style.display = "block"
+    //         })
+    //     } else {
+    //         tutoresCt.style.display = "none";
+    //         inputTutor.forEach(input => {
+    //             input.style.display = "none";
+    //             input.value = "";
+    //         })
+    //     }
+    // })
 
 })
 
